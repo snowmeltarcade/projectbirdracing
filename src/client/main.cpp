471e72a7515ed8b93ed0213f8e36528d4a6973d1
Richard Hughes <richard.j.hughes@hotmail.com>
2021-11-18 21:33:16 -0500

    Add `RelWithDebInfo` build mode.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,7 +4,20 @@
 #include "version.h"
 
 int main(int argv, char* args[]) {
-    std::cout << "Starting client...\n" << PROJECT_NAME << '\n' << PROJECT_NAME << '\n';
+    std::cout << "Starting client...\nProject Name: " << PROJECT_NAME << "\nVersion: " << PROJECT_VERSION << '\n';
+
+    auto mode { "UNKNOWN" };
+    // doesn't seem clang supports `#elifdef` yet: https://reviews.llvm.org/D101192
+#ifdef DEBUG
+    mode = "DEBUG";
+#endif
+#ifdef RELEASE_WITH_DEBUG_INFO
+    mode = "RELEASE WITH DEBUG INFO";
+#endif
+#ifdef RELEASE
+    mode = "RELEASE";
+#endif
+    std::cout << "Running in " << mode << '\n';
 
     std::vector<std::string> arguments;
     for (auto i {0}; i < argv; ++i) {
