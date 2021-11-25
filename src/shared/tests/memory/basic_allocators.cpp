16cd25d819d92185165c2700f3f3ffd69036bcc6
Richard Hughes <richard.j.hughes@hotmail.com>
2021-11-24 23:02:36 -0500

    Add test for `get_number_of_allocated_bytes()`.

--- src/shared/tests/memory/basic_allocators.cpp
+++ src/shared/tests/memory/basic_allocators.cpp
@@ -49,3 +49,22 @@ TEST_CASE("delete - decreases number of bytes allocated", "[shared/memory]") {
 
     REQUIRE(result == expected);
 }
+
+//////////
+/// get_number_of_allocated_bytes
+//////////
+
+TEST_CASE("get_number_of_allocated_bytes - returns number of allocated bytes", "[shared/memory]") {
+    auto start = get_number_of_allocated_bytes();
+
+    auto size = 10;
+    auto ptr = new int[size];
+
+    auto end = get_number_of_allocated_bytes();
+
+    auto result = end - start;
+
+    REQUIRE(sizeof(int) * size == result);
+
+    delete[] ptr;
+}
