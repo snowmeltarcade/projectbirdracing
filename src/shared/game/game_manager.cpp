a800667e85510b8c048a2f97f9699176391f5293
Richard Hughes <richard.j.hughes@hotmail.com>
2022-01-17 21:21:33 -0500

    Start to setup graphics thread.

--- src/shared/game/game_manager.cpp
+++ src/shared/game/game_manager.cpp
@@ -47,28 +47,32 @@ namespace pbr::shared::game {
                                         apis::logging::log_levels::info,
                                         "Game");
 
+//        std::thread graphics_thread(&game_manager::run_graphics_manager,
+//                                    this->_graphics_manager,
+//                                    this->_has_exit_been_requested,
+//                                    this->_frame_synchronize_semaphore,
+//                                    this->_graphics_synchronize_semaphore);
+        std::thread graphics_thread(&game_manager::f);
+
         while (!this->_has_exit_been_requested) {
-            if (!this->_window_manager->update()) {
-                this->_log_manager->log_message("Failed to update window manager.",
+            this->begin_frame();
+
+            if (!this->update_frame()) {
+                this->_log_manager->log_message("Failed to update frame.",
                                                 apis::logging::log_levels::error,
                                                 "Game");
                 return false;
             }
 
-            if (this->_window_manager->should_quit()) {
-                this->request_exit();
-                continue;
-            }
+            this->enter_synchronize_frame();
 
-            if (!this->_scene_manager->run()) {
-                this->_log_manager->log_message("Failed to run scene manager.",
-                                                apis::logging::log_levels::error,
-                                                "Game");
-                return false;
-            }
+            this->synchronize_frame();
 
-            // this will be moved to another thread soon
-            this->_graphics_manager->submit_frame_for_render();
+            this->exit_synchronize_frame();
+        }
+
+        if (graphics_thread.joinable()) {
+            graphics_thread.join();
         }
 
         this->_log_manager->log_message("Finished running the game manager.",
@@ -88,5 +92,71 @@ namespace pbr::shared::game {
                                         "Game");
 
         return true;
+    }
+
+    void game_manager::request_exit() noexcept {
+        this->_log_manager->log_message("Requesting exit from game manager...",
+                                        apis::logging::log_levels::info,
+                                        "Game");
+
+        this->_has_exit_been_requested = true;
+    }
+
+    void game_manager::begin_frame() noexcept {
+        // increase frame count
+    }
+
+    bool game_manager::update_frame() noexcept {
+        if (!this->_window_manager->update()) {
+            this->_log_manager->log_message("Failed to update window manager.",
+                                            apis::logging::log_levels::error,
+                                            "Game");
+            return false;
+        }
+
+        if (this->_window_manager->should_quit()) {
+            this->request_exit();
+            return true;
+        }
+
+        if (!this->_scene_manager->run()) {
+            this->_log_manager->log_message("Failed to run scene manager.",
+                                            apis::logging::log_levels::error,
+                                            "Game");
+            return false;
+        }
+
+        return true;
+    }
+
+    void game_manager::enter_synchronize_frame() noexcept {
+        // wait for the graphics thread to be done with the graphics manager
+        this->_graphics_synchronize_semaphore.acquire();
+    }
+
+    void game_manager::synchronize_frame() noexcept {
+        // submit renderable entities
+        // ...
+        // this->_graphics_manager->submit_renderable_entities()
+    }
+
+    void game_manager::exit_synchronize_frame() noexcept {
+        // let the other game loop threads know we are done with their stuff
+        this->_frame_synchronize_semaphore.release();
+    }
+
+    void game_manager::run_graphics_manager(std::shared_ptr<apis::graphics::igraphics_manager> graphics_manager,
+                                            std::atomic_bool& has_exit_been_requested,
+                                            std::binary_semaphore& frame_synchronize_semaphore,
+                                            std::binary_semaphore& graphics_synchronize_semaphore) noexcept {
+        while (!has_exit_been_requested) {
+            // wait for the logic thread to be finished with the graphics manager
+            frame_synchronize_semaphore.acquire();
+
+            graphics_manager->submit_frame_for_render();
+
+            // tell the logic thread we are done using the graphics manager for now
+            graphics_synchronize_semaphore.release();
+        }
     }
 }
--- src/shared/game/game_manager.h
+++ src/shared/game/game_manager.h
@@ -11,6 +11,8 @@
 #include <memory>
 #include <atomic>
 #include <filesystem>
+#include <thread>
+#include <semaphore>
 
 namespace pbr::shared::game {
     /// Provides the skeleton of the game. The managers to be used are first configured and then added
@@ -64,7 +66,8 @@ namespace pbr::shared::game {
         [[nodiscard]]
         bool initialize() noexcept;
 
-        /// Runs the game. This is a blocking function. It will exit when the game exits
+        /// Runs the game. This is a blocking function. It will exit when the game exits.
+        /// Other needed threads, such as the graphics thread, are started in this function.
         /// \returns `true` upon success, else `false`
         [[nodiscard]]
         bool run() noexcept;
@@ -89,7 +92,13 @@ namespace pbr::shared::game {
         std::shared_ptr<scene::iscene_manager> _scene_manager;
 
         /// Has an exit been requested?
-        std::atomic_bool _has_exit_been_requested {false};
+        std::atomic_bool _has_exit_been_requested { false };
+
+        /// Used to synchronize the main logic thread with the other game loop threads
+        std::binary_semaphore _frame_synchronize_semaphore { 0 };
+
+        /// Used to synchronize the graphics thread with the main logic thread
+        std::binary_semaphore _graphics_synchronize_semaphore { 0 };
 
         /// Shuts down the game
         /// \returns `true` upon success, else `false`
@@ -97,12 +106,39 @@ namespace pbr::shared::game {
         bool shutdown() noexcept;
 
         /// Requests this game manager exits
-        void request_exit() noexcept {
-            this->_log_manager->log_message("Requesting exit from game manager...",
-                                            apis::logging::log_levels::info,
-                                            "Game");
+        void request_exit() noexcept;
 
-            this->_has_exit_been_requested = true;
-        }
+        /// Sets up a new frame
+        void begin_frame() noexcept;
+
+        /// Updates any frame logic
+        /// \returns `true` upon success, else `false`
+        [[nodiscard]]
+        bool update_frame() noexcept;
+
+        /// Enters the frame synchronize state so data can be submitted to other threads
+        void enter_synchronize_frame() noexcept;
+
+        /// Synchronizes data with other threads, such as submitting renderable entities to
+        /// the graphics manager
+        void synchronize_frame() noexcept;
+
+        /// Exits the frame synchronize state
+        void exit_synchronize_frame() noexcept;
+
+        /// Runs the graphics manager on a separate thread.
+        /// This function will only exit if `_has_exit_been_requested` is `true`.
+        /// All access to the graphics manager should only occur after `enter_synchronize_frame()`
+        /// is called and before `exit_synchronize_frame()` is called.
+        /// \param graphics_manager The graphics manager to run
+        /// \param has_exit_been_requested Will be set to `true` if this thread should exit
+        /// \param frame_synchronize_semaphore Used to synchronize the logic thread with the graphics thread
+        /// \param graphics_synchronize_semaphore Used to synchronize the graphics thread with the logic thread
+        static void run_graphics_manager(std::shared_ptr<apis::graphics::igraphics_manager> graphics_manager,
+                                         std::atomic_bool& has_exit_been_requested,
+                                         std::binary_semaphore& frame_synchronize_semaphore,
+                                         std::binary_semaphore& graphics_synchronize_semaphore) noexcept;
+
+        static void f()noexcept{}
     };
 }
