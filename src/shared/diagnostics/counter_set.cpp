4ccd7965efc442177c31ecf64a063a3a490fd40e
Richard Hughes <richard.j.hughes@hotmail.com>
2022-01-20 21:38:21 -0500

    Set implementing counter set functions.

--- src/shared/diagnostics/counter_set.cpp
+++ src/shared/diagnostics/counter_set.cpp
@@ -1,11 +1,34 @@
 #include "counter_set.h"
 
 namespace pbr::shared::diagnostics {
-    void counter_set::increment(const std::string&, int) noexcept {
-
+    void counter_set::increment_counter(const std::string& key, int amount) noexcept {
+        this->_counters[key] += amount;
     }
 
-    void counter_set::decrement(const std::string&, int) noexcept {
+    void counter_set::decrement_counter(const std::string& key, int amount) noexcept {
+        this->_counters[key] -= amount;
+    }
 
+    void counter_set::add_value_to_list(const std::string& key, int value) noexcept {
+        this->_counter_lists[key].push_back(value);
+    }
+
+    int counter_set::get_counter_for_duration(const std::string& key,
+                                              std::chrono::milliseconds duration) noexcept {
+        auto now = std::chrono::system_clock::now();
+        auto last_update = this->_counter_update_times[key];
+
+        if (now - last_update > duration) {
+            this->_counter_update_times[key] = now;
+            this->_counters[key] = 0;
+        }
+
+        auto counter = this->_counters[key];
+        return counter;
+    }
+
+    int counter_set::get_average_for_duration(const std::string&,
+                                              std::chrono::milliseconds) noexcept {
+        return 0;
     }
 }
--- src/shared/tests/diagnostics/counter_set.cpp
+++ src/shared/tests/diagnostics/counter_set.cpp
@@ -9,7 +9,17 @@ using namespace pbr::shared::diagnostics;
 //////////
 
 TEST_CASE("increment - valid amount - increments counter", "[shared/diagnostics]") {
+    counter_set c;
 
+    auto key = "key";
+    auto value = 10;
+
+    c.increment_counter(key, value);
+    c.increment_counter(key, value);
+
+    auto result = c.get_counter(key);
+
+    REQUIRE(result == value * 2);
 }
 
 //////////
@@ -17,5 +27,114 @@ TEST_CASE("increment - valid amount - increments counter", "[shared/diagnostics]
 //////////
 
 TEST_CASE("decrement - valid amount - decrements counter", "[shared/diagnostics]") {
+    counter_set c;
 
+    auto key = "key";
+    auto value = 10;
+
+    c.decrement_counter(key, value);
+    c.decrement_counter(key, value);
+
+    auto result = c.get_counter(key);
+
+    REQUIRE(result == -value * 2);
+}
+
+//////////
+/// add_value_to_list
+//////////
+
+TEST_CASE("add_value_to_list - valid value - adds value to list", "[shared/diagnostics]") {
+    counter_set c;
+
+    auto key = "key";
+    auto value = 10;
+    auto value2 = 20;
+
+    c.add_value_to_list(key, value);
+    c.add_value_to_list(key, value2);
+
+    auto result = c.get_values_for_counter_list(key);
+
+    REQUIRE(result.size() == 2);
+    REQUIRE(result[0] == value);
+    REQUIRE(result[1] == value2);
+}
+
+//////////
+/// get_counter
+//////////
+
+TEST_CASE("get_counter - returns counter", "[shared/diagnostics]") {
+    counter_set c;
+
+    auto key = "key";
+    auto value = 10;
+
+    c.increment_counter(key, value);
+
+    auto result = c.get_counter(key);
+    REQUIRE(result == value);
+
+    c.increment_counter(key, value);
+
+    result = c.get_counter(key);
+    REQUIRE(result == value * 2);
+}
+
+//////////
+/// get_values_for_counter_list
+//////////
+
+TEST_CASE("get_values_for_counter_list - returns values", "[shared/diagnostics]") {
+    counter_set c;
+
+    auto key = "key";
+    auto value = 10;
+    auto value2 = 20;
+
+    c.add_value_to_list(key, value);
+
+    auto result = c.get_values_for_counter_list(key);
+    REQUIRE(result.size() == 1);
+    REQUIRE(result[0] == value);
+
+    c.add_value_to_list(key, value2);
+
+    result = c.get_values_for_counter_list(key);
+    REQUIRE(result.size() == 2);
+    REQUIRE(result[0] == value);
+    REQUIRE(result[1] == value2);
+}
+
+//////////
+/// get_counter_for_duration
+//////////
+
+TEST_CASE("get_counter_for_duration - returns values", "[shared/diagnostics]") {
+    counter_set c;
+
+    auto key = "key";
+    auto value = 10;
+
+    auto now = std::chrono::system_clock::now();
+    auto duration = std::chrono::milliseconds(500);
+
+    auto expected {0};
+
+    // ensure the last update time is set up correctly
+    c.get_counter_for_duration(key, duration);
+
+    while (std::chrono::system_clock::now() - now <= duration) {
+        expected += value;
+
+        c.increment_counter(key, value);
+
+        auto result = c.get_counter_for_duration(key, duration);
+        REQUIRE(result == expected);
+    }
+
+    // the duration has now passed
+    auto passed_result = c.get_counter_for_duration(key, duration);
+    REQUIRE(passed_result == 0);
 }
